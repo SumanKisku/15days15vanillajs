b94583bbf1c9a65f8ea9fef6113fdfb95e203ef1
Suman Kisku <sumankisku1@gmail>
2022-12-15 02:29:34 +0530

    Day 3 - Reviews

---
+++ Day 3 - Reviews/script.js
@@ -0,0 +1,85 @@
+// reviews
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      about:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      about:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      about:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      about:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+
+// selecting element
+let photo = document.getElementById("photo");
+let name = document.getElementById("name");
+let job = document.getElementById("job");
+let about = document.getElementById("about");
+let prevBtn = document.getElementById("prev-btn");
+let nextBtn = document.getElementById("next-btn");
+let surprise = document.getElementById("surprise");
+
+// function to set the reviews
+
+
+// initializing the first review
+let reviewNumber = 0;
+window.addEventListener('DOMContentLoaded', () => {
+    setReview();
+});
+
+
+// prev button: go to the previous review
+
+prevBtn.addEventListener('click', ()=> {
+    reviewNumber--;
+    if(reviewNumber < 0) {
+        reviewNumber = reviews.length - 1;
+    }
+    setReview();
+})
+
+// next button: go to the next review
+
+nextBtn.addEventListener('click', ()=> {
+    reviewNumber++;
+    if(reviewNumber > reviews.length - 1) {
+        reviewNumber = 0;
+    }
+    setReview();
+})
+
+// surpise button: select a random review
+surprise.addEventListener('click', ()=> {
+    reviewNumber = Math.floor(Math.random() * reviews.length);
+    setReview();
+    console.log(reviewNumber);
+})
